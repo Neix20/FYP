5e7209bc32bb514b83787b2e83437f6949a4e299
Neix20 <66442152+Neix20@users.noreply.github.com>
2022-01-13 17:44:49 +0800

    Completed Network Graph
    Created Network Graph From Correlation Matrix

---
+++ Visualization/model_graph.py
@@ -0,0 +1,102 @@
+import pandas as pd
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+def get_df_type(clf_report_arr, col_name):
+    df = pd.DataFrame()
+
+    for name, clf_report in clf_report_arr:
+        df[name] = clf_report[col_name.lower()][:-3]
+    
+    # Replace NA value with 0
+    df.fillna(0, inplace=True)
+    return df
+
+def pfr_graph(df, x_label, y_label, title):
+    fig = px.bar(df.T, x=df.columns, y=df.index, barmode="group")
+    fig.update_traces(texttemplate='%{value:.4f}', textposition='outside')
+    fig.update_layout(
+        title = {
+            'text': title,
+            'x':0.5,
+            "y": 0.85,
+            'xanchor': 'center'
+        },
+        xaxis_title=x_label,
+        yaxis_title=y_label,
+        legend_title="Legend",
+    )
+    fig.update_yaxes(range=[0, 1])
+    fig.show("notebook")
+    
+def prc_roc_graph(model_arr, X, Y, x_cor, title, x_title, y_title, func):
+    # Empty Plot
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x_cor, line=dict(dash='dash'), name="", showlegend=False))
+    
+    for name, model in model_arr:
+        yhat = model.predict_proba(X)
+        yhat = yhat[:, 1]
+        a, b, c = func(Y, yhat)
+        if x_cor == [1, 0]:
+            x_val, y_val = b, a
+        else:
+            x_val, y_val = a, b
+        fig.add_trace(
+            go.Scatter(
+                x = x_val, 
+                y = y_val, 
+                name = name,
+                mode='lines'
+            )
+        )
+        
+    fig.update_layout(
+        title = {
+            'text': title,
+            'x': 0.5,
+            "y": 0.85,
+            'xanchor': 'center'
+        },
+        xaxis_title = x_title,
+        yaxis_title = y_title,
+        legend_title = "Legend",
+    )
+    
+    fig.update_xaxes(range=[0,1])
+    fig.update_yaxes(range=[0,1])
+    fig.show("notebook")
+    
+def acc_graph(acc_arr, title, x_title, y_title):
+    # Empty Plots
+    fig = go.Figure()
+    
+    for name, acc in acc_arr:
+        fig.add_trace(
+            go.Bar(
+                x = [acc], 
+                y = [name],
+                name = name,
+                orientation = 'h'
+            )
+        )
+        
+    fig.update_traces(
+        texttemplate='%{value:.2f}%', 
+        textposition='outside'
+    )
+    
+    fig.update_layout(
+        title = {
+            'text': title,
+            'x': 0.5,
+            'y': 0.85,
+            'xanchor': 'center'
+        },
+        xaxis_title = x_title,
+        yaxis_title = y_title,
+        legend_title = "Legend",
+    )
+    fig.update_xaxes(range=[0,100])
+    fig.show("notebook")
---
+++ Visualization/network_graph.py
@@ -0,0 +1,152 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+from Utilities.corr_matrix import *
+
+def create_graph(corr_matrix):
+    corr_matrix.columns = ["asset_1", "asset_2", "correlation"]
+    
+    # create a new graph from edge list
+    Gx = nx.from_pandas_edgelist(edges, "asset_1", "asset_2", edge_attr=["correlation"])
+
+    # Remove Nodes that are isolated
+    Gx.remove_nodes_from(list(nx.isolates(Gx)))
+
+    # Remove Smaller Tree Graph
+    cmp_len_arr = list(map(lambda x : len(x), nx.connected_components(Gx)))
+
+    for component in list(nx.connected_components(Gx)):
+        if len(component) < max(cmp_len_arr):
+            for node in component:
+                Gx.remove_node(node)
+                
+    return Gx
+
+def assign_colour(correlation):
+    return "#ffa09b" if correlation <= 0 else "#9eccb7"
+
+def assign_thickness(correlation, benchmark_thickness = 2, scaling_factor = 3):
+    return benchmark_thickness * abs(correlation) ** scaling_factor
+
+def assign_node_size(degree, scaling_factor=50):
+    return degree * scaling_factor
+
+def get_edge_width(Gx):
+    edge_witdth = []
+    
+    for key, value in nx.get_edge_attributes(Gx, "correlation").items():
+        edge_width.append(assign_thickness(value))
+    
+    return edge_width
+
+def get_edge_color(Gx):
+    # assign edge colours
+    edge_colours = []
+    
+    for key, value in nx.get_edge_attributes(Gx, "correlation").items():
+        edge_colours.append(assign_colour(value))
+        
+    return edge_colours
+        
+def get_node_size(Gx):
+    # assign node size depending on number of connections (degree)
+    node_size = []
+    
+    for key, value in dict(Gx.degree).items():
+        node_size.append(assign_node_size(value))
+        
+    return node_size
+
+def get_coordinates(G):
+    """Returns the positions of nodes and edges in a format
+    for Plotly to draw the network
+    """
+    # get list of node positions
+    pos = nx.fruchterman_reingold_layout(G)
+
+    Xnodes = [pos[n][0] for n in G.nodes()]
+    Ynodes = [pos[n][1] for n in G.nodes()]
+
+    Xedges = []
+    Yedges = []
+    for e in mst.edges():
+        # x coordinates of the nodes defining the edge e
+        Xedges.extend([pos[e[0]][0], pos[e[1]][0], None])
+        Yedges.extend([pos[e[0]][1], pos[e[1]][1], None])
+
+    return Xnodes, Ynodes, Xedges, Yedges
+
+def network_graph(corr_matrix, title):
+    # Create Basic Graph from Correlation Matrix
+    Gx = create_graph(corr_matrix)
+    
+    # Make Graph into Minimum Spanning Tree
+    mst = nx.minimum_spanning_tree(Gx)
+    
+    # Get Edge Colours
+    edge_colours = get_edge_color(mst)
+    
+    # Get Edge Width
+    edge_widths = get_edge_width(mst)
+    
+    # Get Node Size
+    node_sizes = get_node_size(mst)
+    
+    # Get Node Labels
+    node_label = list(mst.nodes())
+    
+    # edges
+    tracer = go.Scatter(
+        x = Xedges,
+        y = Yedges,
+        mode="lines",
+        line=dict(color="#DCDCDC", width=1),
+        hoverinfo="none",
+        showlegend=False,
+    )
+
+    # nodes
+    tracer_marker = go.Scatter(
+        x=Xnodes,
+        y=Ynodes,
+        mode="markers+text",
+        textposition="top center",
+        marker=dict(size=node_size, line=dict(width=1), color=edge_colours),
+        text=node_label,
+        textfont=dict(size=7),
+        showlegend=False,
+    )
+
+    axis_style = dict(
+        title="",
+        titlefont=dict(size=20),
+        showgrid=False,
+        zeroline=False,
+        showline=False,
+        ticks="",
+        showticklabels=False,
+    )
+
+    layout = dict(
+        title=title,
+        width=800,
+        height=800,
+        autosize=False,
+        showlegend=False,
+        xaxis=axis_style,
+        yaxis=axis_style,
+        hovermode="closest",
+        plot_bgcolor="#fff",
+    )
+
+    fig = go.Figure()
+    fig.add_trace(tracer)
+    fig.add_trace(tracer_marker)
+    fig.update_layout(layout)
+
+    fig.show()
+    
